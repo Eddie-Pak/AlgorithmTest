efae5f1fcbf3fbf0d5d291078e9599edbc2ed97b
Eddie-Pak <foalstn@naver.com>
2024-06-11 00:54:56 +0900

    설명추가

--- src/lv1/SquareArea.kt
+++ src/lv1/SquareArea.kt
@@ -1,8 +1,11 @@
 package lv1
 
 fun main(args: Array<String>) {
+    print("사각형 갯수: ")
     val t = readLine()!!.toInt()
     var maxArea = 0
+
+    println("사각형의 가로, 세로를 순서대로 입력해주세요.")
 
     for (i in 1..t) {
         val (w, h) = readLine()!!.split(" ").map { it.toInt() }
@@ -12,5 +15,5 @@ fun main(args: Array<String>) {
         }
     }
 
-    println(maxArea)
+    println("사각형 넓이의 최댓값: $maxArea")
 }
